329328dc22d09859ee138167ec917a0362f07932
OceanicCoderX <oceaniccoderx@users.noreply.github.com>
2026-04-23 00:33:20 +0530

    fix: update DB_PORT and DB_NAME to match Aiven

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ import os
 
 
 DB_HOST     = os.environ.get("DB_HOST", "edulink-db-oceaniccodderx.h.aivencloud.com")
-DB_PORT     = int(os.environ.get("DB_PORT", 24980))
+DB_PORT     = int(os.environ.get("DB_PORT", 24984))
 DB_USER     = os.environ.get("DB_USER", "avnadmin")
 DB_PASSWORD = os.environ.get("DB_PASSWORD") # Set this in Render Dashboard
-DB_NAME     = os.environ.get("DB_NAME", "edulink")
+DB_NAME     = os.environ.get("DB_NAME", "defaultdb")
 
 # --- Flask Config ---
 SECRET_KEY  = "edulink_secret_key_change_this_in_production"
